fee413ed966a699c6ca2eee210cfa282ace408a9
arifff-id <64384383+arifff-id@users.noreply.github.com>
2020-04-28 17:31:33 +0700

    Membuat file Isal.sh

---
+++ Isal.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo  -n "Masukkan password Anda Di Sini :"
+read pwd ;
+
+if  [ $pwd = "arifisal" ]
+then 
+echo "\033[33;1m' "Password Anda Bener"
+else 
+echo "\033[32;1m' "Password Anda Salah"
+echo "\033[35;1m' "Silahkan Masukkan Lagi"
+fi
+
+echo "green='\033[32;1m'  
+echo "purple='\033[35;1m'
+echo "cyan='\033[36;1m'
+echo "red='\033[31;1m'
+echo "white='\033[37;1m'                                           
+echo "yellow='\033[33;1m'
+
+
+if [ -x /data/data/com.termux/files/usr/libexec/termux/command->        command_not_found_handle() {                                            /data/data/com.termux/files/usr/libexec/termux/>
+        }
+fi
+
+PS1='\$ '
+
+cowsay -f www.cow welcome | lolcat
+
+echo "==================================================================================="
+echo  "\033[36;1m' "Name = "faisal"
+echo  "\033[33;1m' "Alamat = "cirebon"
+echo  "\033[31;1m' "Team = "ngawur"
+echo  "\033[37;1m' "No wa = "085xxxxxxxxx"
+echo "===================================================================================="
+sleep 2
+echo
